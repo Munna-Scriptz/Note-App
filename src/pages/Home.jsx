09944ecbbcbc926d46ef268a5eca7cbec2a05fa2
Munna-Scripts <munnascriptz@gmail.com>
2025-08-27 20:18:48 +0600

    Add note input button added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,14 @@ const Home = () => {
     <div className="min-h-screen bg-gray-100 dark:bg-[#202124] transition-colors pt-6">
       {/*------------- Add Note Input --------------------*/}
 
-      <div className="flex items-center w-[80%] max-w-2xl mx-auto bg-white dark:bg-[#2d2e30] rounded-lg px-4 py-3 shadow-md">
-        <FaStickyNote className="text-gray-500 mr-3" />
-        <input type="text" placeholder="Take a note..." className="bg-transparent w-full outline-none dark:text-white"/>
+      <div className="relative w-[80%] max-w-2xl mx-auto bg-[#2d2e30] rounded-lg px-4 py-3 shadow-md">
+        <div className="flex items-center">
+          <FaStickyNote className="text-gray-500 mr-3" />
+          <input type="text" placeholder="Take a note..." className="bg-transparent w-full outline-none dark:text-white"/>
+          <button className="absolute top-0 right-0 bg-white h-full w-[110px] cursor-pointer text-[#202124] font-medium rounded-lg">
+            Add Note
+          </button>
+        </div>
       </div>
       
       {/* -----------Added Notes-------------- */}

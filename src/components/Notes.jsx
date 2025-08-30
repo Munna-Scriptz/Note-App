cacc8a66a2c77a8b33dea163c23d5a45b879ff59
Munna-Scripts <munnascriptz@gmail.com>
2025-08-30 12:54:29 +0600

    Notes Remomoved

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { RiDeleteBin6Line } from 'react-icons/ri'
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, remove, set, push } from "firebase/database";
 
 
 const Notes = () => {
@@ -19,7 +19,16 @@ const Notes = () => {
 
   } , [])
 
-
+  // ---------------------Delete Note-----------------
+  const handleDel = (Data)=>{
+    set(push(ref(db, 'removedNotes/')), {
+      title: Data.notes.title,
+      content: Data.notes.content,
+      color: Data.notes.color
+    });
+    // ------------Remove 
+    remove(ref(db , 'AllNotes/' + Data.key))
+  }
   return (
     <>
     {
@@ -36,7 +45,7 @@ const Notes = () => {
           {
             noteItem.map((item , i)=>(
               <div key={i} className="bg-[#2d2e30] p-5 rounded-lg shadow hover:shadow-lg relative">
-                <div className='absolute top-4 right-4 cursor-pointer hover:bg-[#e0070780] duration-200 w-[40px] h-[40px] rounded-full flex items-center justify-center'><RiDeleteBin6Line className='text-white text-[20px]'/></div>
+                <div onClick={()=>handleDel(item)} className='absolute top-4 right-4 cursor-pointer hover:bg-[#e0070780] duration-200 w-[40px] h-[40px] rounded-full flex items-center justify-center'><RiDeleteBin6Line className='text-white text-[20px]'/></div>
                 <h3 className="font-bold mb-4 text-white">{item.notes.title}</h3>
                 <p className="text-gray-300">{item.notes.content}</p>
               </div>

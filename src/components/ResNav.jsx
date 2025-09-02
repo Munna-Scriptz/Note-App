708d0d286aef67f7945fbeb5a39abeead645d526
Munna-Scripts <munnascriptz@gmail.com>
2025-09-02 21:47:22 +0600

    Functional Menu Adde

--- src/components/ResNav.jsx
+++ src/components/ResNav.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { FaBell, FaMoon, FaSearch, FaSignOutAlt, FaUserCircle } from 'react-icons/fa'
+import React, { useState } from 'react'
+import { FaAngleLeft, FaBell, FaMoon, FaSearch, FaSignOutAlt, FaUserCircle } from 'react-icons/fa'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router'
 import { userInfo } from '../slice/LoginInfoSlice'
@@ -20,6 +20,11 @@ const ResNav = () => {
         localStorage.removeItem('userInfo')
     }
 
+
+    // -------------Menu State----------------
+    const [menu, setMenu] = useState(true)
+
+
   return (
     <>
     <nav className="lg:hidden flex items-center justify-between px-[16px] py-5 bg-[#2d2e30] shadow-md sticky top-0 z-50">
@@ -36,9 +41,31 @@ const ResNav = () => {
         {/* Icons */}
         <div className='flex items-center gap-3'>
             <IoSunny size={25} className='cursor-pointer'/>
-            <FaBarsStaggered size={25} className='cursor-pointer' />
+            <FaBarsStaggered onClick={()=>setMenu(!menu)} size={25} className='cursor-pointer' />
         </div>
     </nav>
+    {/* --------------------------Menu-------------------------- */}
+    {/* Mobile Menu (Bottom Sheet) */}
+    <div className={`lg:hidden fixed inset-0 z-[100] transition-opacity ${menu ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}>
+        {/* Black overlay – covers full screen */}
+        <div className="absolute inset-0 bg-black/60" onClick={() => setMenu(false)}/>
+
+        {/* Panel */}
+        <section className={`absolute bottom-0 left-0 w-full h-[90vh] bg-[#3C3D3F] rounded-t-2xl p-3 pb-[50px] overflow-y-auto transition-transform duration-500 ${ menu ? "translate-y-0" : "translate-y-full" }`}>
+            {/* ------------Top Buttons----------- */}
+            <div className="flex items-center justify-between mt-1 font-Poppins">
+                <button onClick={() => setMenu(false)} className="text-white text-[25px] cursor-pointer">
+                    <FaAngleLeft />
+                </button>
+                <p className="text-white text-lg font-medium">Menu</p>
+            </div>
+            
+            {/* your menu content here */}
+            <div>
+                
+            </div>
+        </section>
+    </div>
     </>
   )
 }
